178c47ad5c65668d5bfa09d6813e57035174763f
KimWonhee626 <kwh7739@gmail.com>
2023-03-29 15:52:07 +0900

    setting

---
+++ src/main/java/com/example/likegram/LikegramApplication.java
@@ -0,0 +1,13 @@
+package com.example.likegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/likegram/LikegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
