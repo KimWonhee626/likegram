61521614a3f28e33dfac27572b7edbc20baeb679
KimWonhee626 <kwh7739@gmail.com>
2023-03-30 14:49:47 +0900

    유효성 검사, 로그인 폼 구현

--- src/main/java/com/example/likegram/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/example/likegram/boundedContext/member/controller/MemberController.java
@@ -1,7 +1,13 @@
 package com.example.likegram.boundedContext.member.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +16,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+
     @GetMapping("/join")
     public String showJoin() {
         return "user/member/join";
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class JoinForm {
+        @NotBlank
+        @Size(min = 4, max = 30)
+        private final String username;
+        @NotBlank
+        @Size(min = 4, max = 30)
+        private final String password;
+    }
+
     @PostMapping("/join")
-    public String join() {
+    public String join(@Valid JoinForm joinForm) {
         return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String showLogin() {
+        return "user/member/login";
     }
 }
---
+++ src/main/java/com/example/likegram/boundedContext/member/entity/Member.java
@@ -0,0 +1,53 @@
+package com.example.likegram.boundedContext.member.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+
+    private String username;
+
+    private String password;
+
+    public List<? extends GrantedAuthority> getGrantedAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        grantedAuthorities.add(new SimpleGrantedAuthority("member"));
+
+        if ("admin".equals(username)) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
+        }
+
+        return grantedAuthorities;
+    }
+}
